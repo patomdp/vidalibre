f9c7b0a1997aa8f2791bfe203054a04c5dce4b15
Patricio Mariano <34042558+patomdp@users.noreply.github.com>
2023-08-22 13:04:29 +0200

    Update redirect-script.js

--- redirect-script.js
+++ redirect-script.js
@@ -2,5 +2,7 @@
 //   window.location.href = "https://vidalibredegluten.com";
 if ((window.location.href || "").includes("netlify.app"))
   window.location.href = "https://vidalibredegluten.com/temporary-closed.html";
-if ((window.location.href || "") === "https://vidalibredegluten.com")
+if ((window.location.href || "").includes("index.html"))
+  window.location.href = "https://vidalibredegluten.com/temporary-closed.html";
+if ((window.location.href || "") ===  "/")
   window.location.href = "https://vidalibredegluten.com/temporary-closed.html";
