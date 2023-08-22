584f1d293580847fd0c64a1d3a1239f00f26a0a3
Patricio Mariano <34042558+patomdp@users.noreply.github.com>
2023-08-22 12:56:35 +0200

    Update redirect-script.js
    updated to redirect to temporary-closed

--- redirect-script.js
+++ redirect-script.js
@@ -1,2 +1,6 @@
+// if ((window.location.href || "").includes("netlify.app"))
+//   window.location.href = "https://vidalibredegluten.com";
 if ((window.location.href || "").includes("netlify.app"))
-  window.location.href = "https://vidalibredegluten.com";
+  window.location.href = "https://vidalibredegluten.com/temporary-closed.html";
+if ((window.location.href || "") === "https://vidalibredegluten.com")
+  window.location.href = "https://vidalibredegluten.com/temporary-closed.html";
